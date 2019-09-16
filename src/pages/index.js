a495a2364998abfb9241701a265566f9de3a2e5c
Milos <skapha@gmail.com>
2019-09-16 17:12:35 +0200

    snipcart setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection";
 import Info from "../components/Home/info";
 import Menu from "../components/Home/Menu";
+import Products from "../components/Home/Products";
+import Contact from "../components/Home/Contact";
 
 
 const IndexPage = ({data}) => (
@@ -14,6 +16,8 @@ const IndexPage = ({data}) => (
     <BackgroundSection img={data.img.childImageSharp.fluid} title="regular fantja's" styleClass="default-background"></BackgroundSection>
     <Info />
     <Menu items={data.menu} />
+    <Products />
+    <Contact />
   </Layout>
 )
 
